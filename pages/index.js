9406ed07f62194f9d410fd486dc1742c32c4cdfe
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-26 05:50:51 +0600

    dark mode implemented

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
+import useTheme from "next-theme";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [mounted, setMounted] = useState(false);
+
+  const { systemTheme, theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  const themeChanger = () => {
+    if (!mounted) return null;
+    const currentTheme = theme === "system" ? systemTheme : theme;
+    if (currentTheme === "dark") {
+      setTheme("light");
+    } else {
+      setTheme("dark");
+    }
+  };
   return (
-    <div>
-      <div className="bg-red-500">next app</div>
+    <div className="text-gray-700 dark:text-white dark:bg-gray-700 bg-gray-200">
+      <div>next app</div>
+      <button onClick={themeChanger}>Change Theme</button>
     </div>
   );
 }
