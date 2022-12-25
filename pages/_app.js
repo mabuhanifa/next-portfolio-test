25ec4c6e4759d59e22c56e1124f7ba16bd5e2c20
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-26 05:50:59 +0600

    darkMode

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import '../styles/globals.css'
+import { ThemeProvider } from "next-theme";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider enableSystem={true} attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
