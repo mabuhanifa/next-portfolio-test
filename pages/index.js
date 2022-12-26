c33e7ab75cdc063727807742769b4c92332788c1
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-27 04:01:35 +0600

    main

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
+import Main from "../components/Main";
 import Nav from "../components/Nav";
+
 
 export default function Home() {
 
   return (
-    <div className="text-gray-700 dark:text-white dark:bg-gray-700 bg-gray-200">
+    <div className="text-gray-700 dark:text-white dark:bg-gray-700 bg-gray-100 py-5 px-20">
       <Nav/>
+      <Main/>
     </div>
   );
 }
