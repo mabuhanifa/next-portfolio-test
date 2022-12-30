a305e984410dacad2b17fc701947036de533acb6
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-31 05:16:49 +0600

    data updated

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,10 +1,15 @@
-import { useRouter } from 'next/router';
-import React from 'react';
+import { useRouter } from "next/router";
+import React from "react";
+import { data } from "../../data/data";
 
 export default function Project() {
-    const {query:{id}} = useRouter();
-    console.log(id);
+  const {
+    query: { id },
+  } = useRouter();
   return (
-    <div>Project {id}</div>
-  )
+    <div>
+      Project {id}
+      <div>{data.map}</div>
+    </div>
+  );
 }
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,5 +1,20 @@
+import Link from "next/link";
 import React from "react";
+import { data } from "../../data/data";
 
 export default function Projects() {
-  return <div className="mx-20">projects</div>;
+  return (
+    <div className="mx-20">
+      <div>
+        {data.map((data, i) => {
+          return (
+            <div key={i}>
+              <h1>{data.name}</h1>
+              <Link href={`projects/${data.id}`}>Go</Link>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
