ce4c63fec7561ff86d27d01c07303062771d34a0
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-30 23:58:19 +0600

    updated

--- components/Skills.js
+++ components/Skills.js
@@ -40,7 +40,7 @@ const Skills = () => {
           <span className=" hover:text-indigo-700">
             <SiRedux />
           </span>
-          <span className=" dark:hover:text-white hover:text-gray-400">
+          <span className=" dark:hover:text-gray-200 hover:text-gray-400">
             <SiNextdotjs />
           </span>
           <span className=" hover:text-green-600">
