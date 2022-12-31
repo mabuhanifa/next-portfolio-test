129ee3864483afadb1cbe4915799f1e739d5ef97
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-01 05:35:01 +0600

    projects updated

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -4,18 +4,21 @@ import projects from "../../data/projects";
 
 export default function Projects() {
   return (
-    <div className="mx-40 ">
-      <div className="flex justify-between ">
+    <div className="2xl:mx-40 ">
+      <div className="grid sm:grid-cols-3 gap-3">
         {projects.map((data, i) => {
           return (
             <div
               className="p-5 my-10 bg-white rounded-lg shadow-lg dark:text-gray-300 dark:bg-[#1b232e] dark:shadow-2xl-[#101621]"
               key={i}
             >
-              <h1>{data.name}</h1>
-              <button className="rounded px-20 py-2 bg-gray-700 dark:bg-gray-50 dark:text-gray-700 text-gray-50 font-semibold">
-                <Link href={`projects/${data.id}`}>Details</Link>
-              </button>
+              <h1 className="text-3xl font-bold mb-5">{data.name}</h1>
+              <p className="py-5">{data.info}</p>
+              <div className="flex justify-center">
+                <button className="w-full rounded  py-2 bg-gray-700 dark:bg-gray-50 dark:text-gray-700 text-gray-50 font-semibold">
+                  <Link href={`projects/${data.id}`}>Details</Link>
+                </button>
+              </div>
             </div>
           );
         })}
