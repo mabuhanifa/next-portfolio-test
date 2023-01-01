ed3076c894be01684b7e534d6c75387f23fdecf3
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-02 00:24:01 +0600

    btn updated

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -6,6 +6,7 @@ import projects from "../../data/projects";
 export default function Projects() {
   return (
     <div className="2xl:mx-40 ">
+      <h1 className="text-3xl font-bold text-center my-10">Projects</h1>
       <div className="grid sm:grid-cols-3 gap-5">
         {projects.map((data, i) => {
           return (
@@ -20,15 +21,15 @@ export default function Projects() {
                   alt="img"
                   width={400}
                   height={400}
-                  className="w-96 h-72 object-cover pb-5 rounded"
+                  className="w-96 h-60 object-cover pb-5 rounded"
                 />
               </div>
               <h1 className="text-3xl font-bold ">{data.name}</h1>
               <p className="py-5">{data.info}</p>
               <div className="flex justify-center">
                 <button
-                  className="w-full rounded  py-2 bg-gray-700 dark:bg-gray-50
-                 dark:text-gray-700 text-gray-50 font-semibold"
+                  className="w-full rounded py-2 bg-gray-700 dark:bg-indigo-700
+                 dark:text-gray-100 text-gray-50 font-semibold hover:bg-gray-900 dark:hover:bg-indigo-900"
                 >
                   <Link href={`projects/${data.id}`}>Details</Link>
                 </button>
