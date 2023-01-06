c93c40ffe047497ce5bcd639ecb246a6f15662d9
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-07 00:31:15 +0600

    main classes

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,17 @@
-import { Montserrat } from "@next/font/google";
 import { ThemeProvider } from "next-theme";
 import Footer from "../components/Footer";
 import Nav from "../components/Nav";
 import "../styles/globals.css";
 
-const roboto = Montserrat({
-  subsets: ["latin"],
-  weight: ["400", "500", "700"],
-  variable: "--font-roboto",
-});
+// const roboto = Montserrat({
+//   subsets: ["latin"],
+//   weight: ["400", "500", "700"],
+//   variable: "--font-roboto",
+// });
 
 export default function App({ Component, pageProps }) {
   return (
-    <main >
+    <main className="text-gray-700 dark:text-white dark:bg-gray-700 bg-gray-100 py-5 px-10 sm:px-20">
       {/* className={`${roboto.variable} font-roboto`} */}
       <ThemeProvider enableSystem={true} attribute="class">
         <Nav />
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Projects from "./projects";
 export default function Home() {
 
   return (
-    <div className="text-gray-700 dark:text-white dark:bg-gray-700 bg-gray-100 py-5 px-10 sm:px-20">
+    <div >
       <Front/>
       <Projects/>
     </div>
