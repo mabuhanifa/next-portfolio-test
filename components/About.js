9c45278fe376ec2f1c94f63d0eed7763c39a76e2
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-19 10:15:16 +0600

    about

--- components/About.js
+++ components/About.js
@@ -19,8 +19,7 @@ export default function About() {
         </div>
         <div className="mt-20 col-span-3 p-5 ">
           <p className="text-lg ">
-            I am Mohammed Abu Hanifa, a Full-Stack Web Developer. Mostly I have worked with MERN Stack related technologies. I have
-            a passion for building beautiful and functional web applications. I have a good understanding of the React JS and React State Management. I am always looking to learn new technologies.
+          I am Mohammed Abu Hanifa, a Full-Stack Web Developer. Mostly I have worked with MERN Stack related technologies. I have a passion for building smooth and functional web applications with minimalist design. I have a good understanding of React JS and React State Management. I am always looking to learn new technologies.
           </p>
           <a
             href="https://drive.google.com/file/d/10rhqksoLWDgPd0-wZXtiFzfuNPxnQhr6/view"
