a22f3437b4eebade6494490480890cb20f3c204d
shourovahmd <moh.abuhanifa@gmail.com>
2023-03-18 15:08:04 +0600

    contact width fixed

--- components/Contact.js
+++ components/Contact.js
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div className=" flex flex-col md:flex-row my-20 ">
       <div className="w-full ">
-        <div className="mx-auto text-center sm:text-left w-96 sm:float-right p-10 sm:mr-20">
+        <div className="mx-auto text-center sm:text-left sm:w-96 sm:float-right sm:p-10 sm:mr-20">
           <h1 className="text-3xl mt-20 font-bold">Contact Me</h1>
           <p className="py-5">
             Send me an email if you have any questions or want to work together.
@@ -12,7 +12,7 @@ const Contact = () => {
           </p>
         </div>
       </div>
-      <div className="w-96 m-10 mx-auto md:w-full">
+      <div className="w-80 sm:w-96 m-10 mx-auto md:w-full">
         <ContactForm />
       </div>
     </div>
